2e99049a541c568010e37246f7b9e498aeba5d20
Raghava2292 <raaghava92@gmail.com>
2022-11-02 16:57:16 +0530

    update delete image

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -1,9 +1,21 @@
 const { StatusCodes } = require("http-status-codes")
+const User = require('../model/userModel');
 
 
 const adminAuth = async (req, res, next) => {
     try {
+        const id = req.user.id
 
+        const extUser = await User.findById({_id:id})
+
+        //validate role
+        if(extUser.role !== "superadmin")
+            return res.status(StatusCodes.BAD_REQUEST).json({msg: "Access denied for non-admin users"})
+        
+        next()
+        
+        // res.json({adminAuth: extUser})
+        // res.json({adminAuth: req.user})
     } catch (err) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err.message})
     }
